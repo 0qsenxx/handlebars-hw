fbdf6ded1a30b038ed5e01779cc3dd00d3014039
0qsenxx <semen.antipyuk@gmail.com>
2024-01-26 10:40:12 +0200

    hanlebars hw

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+import Handlebars from "../../node_modules/handlebars";
+import { appleProducts } from "./appleProducts";
+import template from "../template/appleProductsList.hbs";
+
+const markup = template({ items: appleProducts });
+document.querySelector("main").innerHTML = markup;
+
+const lowPticeButtonRef = document.querySelector("[data-filter=low]");
+const highPticeButtonRef = document.querySelector("[data-filter=high]");
+const mainRef = document.querySelector("main");
+
+lowPticeButtonRef.addEventListener("click", () => {
+  const sortProducts = appleProducts.toSorted(
+    (lastProduct, nextProduct) => lastProduct.price - nextProduct.price
+  );
+  const markup = template({ items: sortProducts });
+  mainRef.innerHTML = markup;
+});
+
+highPticeButtonRef.addEventListener("click", () => {
+  const sortProducts = appleProducts.toSorted(
+    (lastProduct, nextProduct) => nextProduct.price - lastProduct.price
+  );
+  const markup = template({ items: sortProducts });
+  mainRef.innerHTML = markup;
+});
